a03588dc14a24de9cdef910503f261069bd4b128
SNik50 <nichi.franceschini@gmail.com>
2024-08-04 19:35:34 +0200

    Initial commit

---
+++ src/main/java/net/nik/familyjewels/FamilyJewelsClient.java
@@ -0,0 +1,10 @@
+package net.nik.familyjewels;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FamilyJewelsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        
+    }
+}
