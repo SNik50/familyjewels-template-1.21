19e9f17d6ead60f08d028814b173521a55be4322
SNik50 <nichi.franceschini@gmail.com>
2024-08-08 17:27:52 +0200

    adding blocks

--- src/main/java/net/nik/familyjewels/item/ModItems.java
+++ src/main/java/net/nik/familyjewels/item/ModItems.java
@@ -8,6 +8,7 @@ import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
+import net.nik.familyjewels.item.custom.ChainsawItem;
 
 import static net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents.*;
 
@@ -16,6 +17,7 @@ public class ModItems {
     public static final Item EXAMPLE_ITEM = registerItem("example_item", new Item(new Item.Settings()));
     public static final Item FLUORITE = registerItem("fluorite", new Item(new Item.Settings()));
     public static final Item RAW_FLUORITE = registerItem("raw_fluorite", new Item(new Item.Settings()));
+    public static final Item CHAINSAW = registerItem("chainsaw", new ChainsawItem(new Item.Settings().maxDamage(32)));
 
     private static void customIngredients(FabricItemGroupEntries entries){
         entries.add(FLUORITE);
